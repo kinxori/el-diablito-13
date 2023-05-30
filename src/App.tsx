f9d2223e2b8b54da22810d804259d33adce40335
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-29 21:36:29 -0600

    testing deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">homelo chino</div>;
+  return (
+    <div className="App">
+      <h2>Hola soy homelo chino</h2>
+      <img style={{ position: "absolute" }} src="./../assets/Pg.png"></img>
+    </div>
+  );
 }
 
 export default App;
