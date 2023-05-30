54896ed455bc194e811d02a33549e68ea0956027
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-30 00:07:11 -0600

    starting with home route

--- routes/home/Home.tsx
+++ routes/home/Home.tsx
@@ -1,7 +1,21 @@
+import { Outlet, useLocation } from "react-router-dom";
+import backgroundImg from "./../../src/assets/BK.png";
+
 export default function () {
+  const location = useLocation();
+
   return (
-    <section>
-      <h2>This is home</h2>
-    </section>
+    <article>
+      <img src={backgroundImg} className="home-bg-img"></img>
+      {location.pathname !== "/" ? (
+        <Outlet />
+      ) : (
+        <section className="home-landing-page">
+          <div className="home-landing-page-content">
+            <div>homelo chino</div>
+          </div>
+        </section>
+      )}
+    </article>
   );
 }
