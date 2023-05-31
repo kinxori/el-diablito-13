5212110c1ebeb623b1543fc49db065e490af3540
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-30 22:31:55 -0600

    implementing slide menu in mobile nav bar

--- components/nav-bar/NavBar.tsx
+++ components/nav-bar/NavBar.tsx
@@ -3,17 +3,17 @@ import NavMobile from "./navMobile";
 import { useState, useEffect } from "react";
 
 export default function NavBar() {
-  const [navComponent, setIsNavComponent] = useState(<NavMobile />);
+  const [navComponent, setNavComponent] = useState(<NavMobile />);
 
   useEffect(() => {
     const handleRezise = () => {
       const screenWidth = window.innerWidth;
       if (screenWidth <= 600) {
-        setIsNavComponent(<NavMobile />);
+        setNavComponent(<NavMobile />);
       } else if (screenWidth <= 1180) {
-        setIsNavComponent(<div>tablet</div>);
+        setNavComponent(<div>tablet</div>);
       } else {
-        setIsNavComponent(<div>desktop</div>);
+        setNavComponent(<div>desktop</div>);
       }
     };
     window.addEventListener("resize", handleRezise);
--- components/nav-bar/navMobile.tsx
+++ components/nav-bar/navMobile.tsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logoNav from "./../../src/assets/BK_f_white.png";
 
 export default function NavMobile() {
+  const [isClicked, setClicked] = useState(false);
+
   return (
     <section className="nav-mobile-root">
       <div className="nav-mobile-content">
         <div className="nav-mobile-slide-root">
-          <span className="nav-mobile-slide-icon">
-            <i className="fa-solid fa-bars"></i>
+          <span className="nav-mobile-slide-icon" onClick={() => setClicked(!isClicked)}>
+            {isClicked ? (
+              <i className="fa-solid fa-xmark"></i>
+            ) : (
+              <i className="fa-solid fa-bars"></i>
+            )}
           </span>
           <div style={{ display: "none" }}>slide</div>
         </div>
