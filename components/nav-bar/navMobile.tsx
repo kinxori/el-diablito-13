7a7f7ad219bfa451e8e677c34a0be0e0014f6452
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-30 22:17:12 -0600

    starting slide menu in mobile nav bar

--- components/nav-bar/navMobile.tsx
+++ components/nav-bar/navMobile.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logoNav from "./../../src/assets/BK_f_white.png";
 
 export default function NavMobile() {
@@ -5,9 +6,14 @@ export default function NavMobile() {
     <section className="nav-mobile-root">
       <div className="nav-mobile-content">
         <div className="nav-mobile-slide-root">
-          <i className="fa-solid fa-bars"></i>
+          <span className="nav-mobile-slide-icon">
+            <i className="fa-solid fa-bars"></i>
+          </span>
+          <div style={{ display: "none" }}>slide</div>
         </div>
-        <img className="nav-mobile-main-logo" src={logoNav} alt="el-diablito-13"></img>
+        <Link to="/">
+          <img className="nav-mobile-main-logo" src={logoNav} alt="el-diablito-13"></img>
+        </Link>
       </div>
     </section>
   );
