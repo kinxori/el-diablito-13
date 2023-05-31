3c0554ae948cb13409d1ffc52e7d35073ad30470
kinxori <116584982+kinxori@users.noreply.github.com>
2023-05-30 22:24:46 -0600

    state managment for navbar media query depending on screen size

--- components/nav-bar/NavBar.tsx
+++ components/nav-bar/NavBar.tsx
@@ -1,10 +1,23 @@
 import "./🎨navBar.css";
 import NavMobile from "./navMobile";
+import { useState, useEffect } from "react";
 
 export default function NavBar() {
-  return (
-    <>
-      <NavMobile />
-    </>
-  );
+  const [navComponent, setIsNavComponent] = useState(<NavMobile />);
+
+  useEffect(() => {
+    const handleRezise = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 600) {
+        setIsNavComponent(<NavMobile />);
+      } else if (screenWidth <= 1180) {
+        setIsNavComponent(<div>tablet</div>);
+      } else {
+        setIsNavComponent(<div>desktop</div>);
+      }
+    };
+    window.addEventListener("resize", handleRezise);
+    return () => window.removeEventListener("resize", handleRezise);
+  }, []);
+  return <>{navComponent}</>;
 }
