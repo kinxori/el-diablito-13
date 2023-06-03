6cde5693bd29a692e58c9bc78d77dedede67b67b
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-02 23:03:29 -0600

    disclaimer files added

---
+++ components/disclaimer-component/disclaimer-comp.tsx
@@ -0,0 +1,21 @@
+import "./🎨disclaimer-comp.css";
+
+export default function DisclaimerComp() {
+  return (
+    <section id="disclaimer-comp-id" className="disclaimer-comp-root">
+      <div className="disclaimer-comp-copy">
+        <h5>Descargo de responsabilidad de "El Diablito 13"</h5>
+        <p>
+          La información en este sitio web es solo para fines informativos. No nos hacemos
+          responsables de la integridad, precisión o actualidad de la información proporcionada, ni
+          de cualquier pérdida o daño derivado de su uso. Los enlaces a otros sitios web se
+          proporcionan como conveniencia, pero no respaldamos ni garantizamos la exactitud de las
+          opiniones o información en esos sitios. Hacemos todo lo posible para mantener el sitio web
+          en funcionamiento, pero no nos hacemos responsables de la disponibilidad temporal debido a
+          problemas técnicos. Para cualquier pregunta o inquietud, contáctanos.
+        </p>
+        <p>Todos los derechos reservados © 2023 "El Diablito 13".</p>
+      </div>
+    </section>
+  );
+}
