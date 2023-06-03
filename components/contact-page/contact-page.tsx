db0b8c1354550c7f4ef5ba001abe94b204f41cfc
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-02 18:26:21 -0600

    email form created

--- components/contact-page/contact-page.tsx
+++ components/contact-page/contact-page.tsx
@@ -1,7 +1,21 @@
 import "./🎨contact-page.css";
 import CompButton from "../custom-buttons/CompButton";
+import EmailForm from "../email-form/email-form";
+import { useRef, useState } from "react";
 
 export default function ContactPage() {
+  const [clipboardIsCopy, setClipboardIsCopy] = useState(false);
+  const textToClipBoard = useRef("") as any;
+
+  const handleClipboardState = () => {
+    const text = textToClipBoard.current.textContent;
+    navigator.clipboard.writeText(text);
+    setClipboardIsCopy(!clipboardIsCopy);
+    setTimeout(() => {
+      setClipboardIsCopy(false);
+    }, 5000);
+  };
+
   return (
     <section id="contact-page-id" className="contact-page-root">
       <div className="contact-page-content">
@@ -24,8 +38,23 @@ export default function ContactPage() {
             </p>
             <CompButton className="contact-page-CTA-button"> Ir a Twitch</CompButton>
           </div>
+          <div className="contact-page-CTA">
+            <h2>
+              Mándame un correo <i className="fa-regular fa-envelope"></i>
+            </h2>
+            <p>Si quieres algun tipo de comision o te interesa colaborar, mandamé un correo!</p>
+            <p ref={textToClipBoard}> gustavoq26@gmail.com </p>
+
+            <CompButton className="contact-page-CTA-button" onClick={handleClipboardState}>
+              {clipboardIsCopy ? (
+                <i className="fa-solid fa-check"></i>
+              ) : (
+                <i className="fa-regular fa-clipboard"></i>
+              )}
+            </CompButton>
+          </div>
         </div>
-        <h1>here goes mail component</h1>
+        <EmailForm />
       </div>
     </section>
   );
