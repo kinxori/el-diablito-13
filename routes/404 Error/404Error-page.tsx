47c787180be98474d9f2d7610a110b2acf823b56
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-05 18:20:43 -0600

    adding 404 page files to repo

---
+++ routes/404 Error/404Error-page.tsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+import error404pageIMG from "./../../src/assets/08_1080.png";
+import "./🎨404error-page.css";
+import LinkButton from "../../components/custom-buttons/LinkButton";
+
+export default function Error404() {
+  const error: any = useRouteError();
+
+  return (
+    <section className="error-404-page-root">
+      <img src={error404pageIMG} alt="error page el diablito 13"></img>
+      <h2>Oops! Error 404 👺</h2>
+      <i>Status: {error.statusText || error.message}</i>
+      <LinkButton className="error-404-page-CTA">Go Home</LinkButton>
+    </section>
+  );
+}
