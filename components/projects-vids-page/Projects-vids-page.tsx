edfccf546877db3d901aca9bc98fd3f4df9e1990
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-07 21:20:05 -0600

    full screen feature added to vids card too

--- components/projects-vids-page/Projects-vids-page.tsx
+++ components/projects-vids-page/Projects-vids-page.tsx
@@ -21,43 +21,101 @@ export default function ProjectsVidsPage() {
         </div>
         <div className="projects-vids-page-cards">
           <CardDisplay header="video 1">
-            <div className="projects-draws-fullscreen-asset" onClick={() => handleFullScreen("1")}>
-              {isLoadingVideo && (
-                <div className="projects-draws-asset-loader">Loading video...</div>
+            <div className="projects-vids-fullscreen-asset" onClick={() => handleFullScreen("1")}>
+              {!isLoadingVideo && (
+                <div className="projects-vids-asset-loader">Loading video...</div>
               )}
               {isFullScreen === "1" && (
-                <LinkButton className="projects-draws-asset-close-button" variant="linkButton">
+                <LinkButton className="projects-vids-asset-close-button" variant="linkButton">
                   Close
                 </LinkButton>
               )}
               <video
-                controls
+                loop
+                autoPlay
                 src={projectsVideo1}
                 className={
-                  isFullScreen === "1" ? "card-display-asset-open" : "card-display-asset-close"
+                  isFullScreen === "1" ? "projects-vids-asset-open" : "projects-vids-asset-close"
                 }
+                onLoadedData={() => setIsLoadingVideo(true)}
               ></video>
-
-              {isFullScreen === "1" && <div className="projects-draws-asset-bg-blank"></div>}
+              {isFullScreen === "1" && <div className="projects-vids-asset-bg-blank"></div>}
             </div>
             <div className="projects-vids-page-CTA">
               <LinkButton>Visitar</LinkButton>
             </div>
           </CardDisplay>
           <CardDisplay header="video 2">
-            <video controls src={projectsVideo1} className="card-display-vid-content"></video>
+            <div className="projects-vids-fullscreen-asset" onClick={() => handleFullScreen("2")}>
+              {!isLoadingVideo && (
+                <div className="projects-vids-asset-loader">Loading video...</div>
+              )}
+              {isFullScreen === "2" && (
+                <LinkButton className="projects-vids-asset-close-button" variant="linkButton">
+                  Close
+                </LinkButton>
+              )}
+              <video
+                loop
+                autoPlay
+                src={projectsVideo1}
+                className={
+                  isFullScreen === "2" ? "projects-vids-asset-open" : "projects-vids-asset-close"
+                }
+                onLoadedData={() => setIsLoadingVideo(true)}
+              ></video>
+              {isFullScreen === "2" && <div className="projects-vids-asset-bg-blank"></div>}
+            </div>
             <div className="projects-vids-page-CTA">
               <LinkButton>Visitar</LinkButton>
             </div>
           </CardDisplay>
           <CardDisplay header="video 3">
-            <video controls src={projectsVideo1} className="card-display-vid-content"></video>
+            <div className="projects-vids-fullscreen-asset" onClick={() => handleFullScreen("3")}>
+              {!isLoadingVideo && (
+                <div className="projects-vids-asset-loader">Loading video...</div>
+              )}
+              {isFullScreen === "3" && (
+                <LinkButton className="projects-vids-asset-close-button" variant="linkButton">
+                  Close
+                </LinkButton>
+              )}
+              <video
+                loop
+                autoPlay
+                src={projectsVideo1}
+                className={
+                  isFullScreen === "3" ? "projects-vids-asset-open" : "projects-vids-asset-close"
+                }
+                onLoadedData={() => setIsLoadingVideo(true)}
+              ></video>
+              {isFullScreen === "3" && <div className="projects-vids-asset-bg-blank"></div>}
+            </div>
             <div className="projects-vids-page-CTA">
               <LinkButton>Visitar</LinkButton>
             </div>
           </CardDisplay>
           <CardDisplay header="video 4">
-            <video controls src={projectsVideo1} className="card-display-vid-content"></video>
+            <div className="projects-vids-fullscreen-asset" onClick={() => handleFullScreen("4")}>
+              {!isLoadingVideo && (
+                <div className="projects-vids-asset-loader">Loading video...</div>
+              )}
+              {isFullScreen === "4" && (
+                <LinkButton className="projects-vids-asset-close-button" variant="linkButton">
+                  Close
+                </LinkButton>
+              )}
+              <video
+                loop
+                autoPlay
+                src={projectsVideo1}
+                className={
+                  isFullScreen === "4" ? "projects-vids-asset-open" : "projects-vids-asset-close"
+                }
+                onLoadedData={() => setIsLoadingVideo(true)}
+              ></video>
+              {isFullScreen === "4" && <div className="projects-vids-asset-bg-blank"></div>}
+            </div>
             <div className="projects-vids-page-CTA">
               <LinkButton>Visitar</LinkButton>
             </div>
