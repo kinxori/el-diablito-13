c4706081b31b08038736d2cb789a9013d2af3995
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-23 18:03:01 -0600

    updated about page content and css for mobile query

--- components/about-page/About-page.tsx
+++ components/about-page/About-page.tsx
@@ -8,15 +8,47 @@ export default function AboutPage() {
       <div className="about-page-content">
         <img src={aboutMainImg} alt="about page el diablito trece"></img>
         <div className="about-page-copy">
-          <h2>¿Quién es El Diablito Trece? 😈</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem ducimus
-            quisquam debitis, quibusdam eligendi ab. Vel, vitae nulla, cumque sequi autem incidunt
-            aliquam quo suscipit, veritatis ipsum sit distinctio est Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Praesentium iusto repellendus beatae aliquam minus quod?
-            Nobis et alias, inventore, ducimus dignissimos quis, quibusdam magni suscipit nesciunt
-            veniam obcaecati dolore quasi! 👺
-          </p>
+          <div className="about-page-copy-content">
+            <h2>¿Quién es El Diablito Trece? 😈</h2>
+            <p>
+              El Diablito 13 es una secta de animadores que se dedica a la animación 2D y 3D. Su
+              enfoque se centra en expresar ideas surrealistas y alucinantes para entretener con una
+              experiencia visual impactante.
+            </p>
+            <img
+              src=""
+              alt="Imagen sobre el estilo del diablito 13"
+              className="about-page-copy-content-img"
+            ></img>
+            <p>
+              Descubrieron su pasión por la animación al darse cuenta de las ideas imposibles en sus
+              mentes y cómo la animación les permite darles vida sin riesgo.
+            </p>
+            <p>
+              Su proceso creativo se basa en buenas pláticas, referencias y diversión. Encuentran
+              inspiración en temas actuales, vivencias y alucinaciones.
+            </p>
+            <img
+              src=""
+              alt="Podrías incluir una imagen que represente una sesión de brainstorming o ilustraciones y referencias que inspiran su creatividad."
+              className="about-page-copy-content-img"
+            ></img>
+            <p>
+              Consideran que el arte siempre lleva un mensaje personal o dirigido a un sector. Ven
+              la relación entre el arte y la tecnología como una adaptación para mejorar su trabajo.
+            </p>
+            <p>
+              Prefieren Instagram para mostrar su trabajo y Facebook para diversión. Si tuvieran que
+              hacer un autorretrato, sería un diablo grotesco y enorme con múltiples caras.
+            </p>
+            <img src="" alt="dibujo de diablo" className="about-page-copy-content-img"></img>
+            <p>
+              Aconsejan perseverancia, representar ideas y estudiar el entorno. Películas favoritas:
+              "Fight Club", "Fear & Loathing in Las Vegas", "Django", "Ren y Stimpy" e "Invader
+              Zim". El Diablito 13 promueve una experiencia única y cautivadora a través de su
+              animación. 👺
+            </p>
+          </div>
           <div className="about-page-CTA">
             <LinkButton>Sigueme en Instagram!</LinkButton>
           </div>
