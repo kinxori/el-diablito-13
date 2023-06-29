728ed3e173eafc46d92d247fcf3531c7c4998721
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-28 22:45:49 -0600

    updated colabs page content - replaced img assets for rezised assets

--- components/colabs-page/Colabs-page.tsx
+++ components/colabs-page/Colabs-page.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./🎨colabs-page.css";
 import LinkButton from "../custom-buttons/LinkButton";
 import CardDisplay from "../card-display/card-display";
-import glotoAsset from "/src/assets/colabs/glotoneria-kawai-poster.png";
-import grenudoAsset from "/src/assets/colabs/grenudo-logo.png";
+import glotoAsset from "/src/assets/colabs/glotoneria-kawai-poster-rezised.png";
+import grenudoAsset from "/src/assets/colabs/grenudo-logo-resized.png";
 import NLPAsset from "/src/assets/colabs/NLP-portada.png";
 
 export default function ColabsPage() {
