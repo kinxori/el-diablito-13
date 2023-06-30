1c717799dd9dbb4d30f6899ee5d1b97266258074
kinxori <116584982+kinxori@users.noreply.github.com>
2023-06-29 18:39:24 -0600

    updated contact page content and css for mobile media query

--- components/contact-page/contact-page.tsx
+++ components/contact-page/contact-page.tsx
@@ -7,56 +7,78 @@ export default function ContactPage() {
   return (
     <section id="contact-page-id" className="contact-page-root">
       <div className="contact-page-content">
-        <div className="contact-page-copy">
-          <h2>
-            Instagram&nbsp;<i className="fa-brands fa-instagram"></i>
-          </h2>
-          <p>Dame un follow en Instagram para seguir mis proyectos o hacer una colaboración.</p>
-        </div>
-        <LinkButton
-          to="https://www.instagram.com/eldiablitotrece/"
-          target="_blank"
-          className="contact-page-CTA-button"
-        >
-          Ir a Instagram
-        </LinkButton>
-        <div className="contact-page-copy">
-          <h2>
-            Twitch.tv&nbsp;<i className="fa-brands fa-twitch"></i>
-          </h2>
+        <div className="contact-page-body">
+          <div className="contact-page-copy">
+            <h2>
+              Instagram&nbsp;<i className="fa-brands fa-instagram"></i>
+            </h2>
+            <p>
+              Dame un follow en Instagram para seguir mis proyectos más actualizados o hacer una
+              colaboración.
+            </p>
+            <LinkButton
+              to="https://www.instagram.com/eldiablitotrece/"
+              target="_blank"
+              className="contact-page-CTA-button"
+            >
+              Ir a Instagram
+            </LinkButton>
+          </div>
+          <div className="contact-page-copy">
+            <h2>
+              Twitch.tv&nbsp;<i className="fa-brands fa-twitch"></i>
+            </h2>
 
-          <p>
-            Si te interesa ver el proceso de 0 a 100 de mis proyectos, no dudes en echarte un
-            clavado a mis livestreams.
-          </p>
-        </div>
-        <LinkButton
-          to="https://www.twitch.tv/eldiablitotrece"
-          target="_blank"
-          className="contact-page-CTA-button"
-        >
-          Ir a Twitch
-        </LinkButton>
-        <div className="contact-page-copy">
-          <h2>
-            Discord&nbsp;<i className="fa-brands fa-discord"></i>
-          </h2>
+            <p>
+              Si te interesa ver el proceso de 0 a 100 de mis proyectos, no dudes en echarte un
+              clavado a mis livestreams.
+            </p>
+            <LinkButton
+              to="https://www.twitch.tv/eldiablitotrece"
+              target="_blank"
+              className="contact-page-CTA-button"
+            >
+              Ir a Twitch
+            </LinkButton>
+          </div>
+          <div className="contact-page-copy">
+            <h2>
+              Discord&nbsp;<i className="fa-brands fa-discord"></i>
+            </h2>
 
-          <p>
-            Te invito a unirte a mi comunidad de Discord, donde compartimos de todo tipo de música,
-            cortos, arte y videojuegos. ¡Bienvenido al Diablito Trece! 👺
-          </p>
+            <p>
+              Te invito a unirte a mi comunidad de Discord, donde compartimos y colaboramos como
+              artistas. ¡Bienvenido al Diablito Trece! 👺
+            </p>
+            <LinkButton
+              to="https://discord.gg/pQAFQxaUWC"
+              target="_blank"
+              className="contact-page-CTA-button"
+            >
+              Ir a Discord
+            </LinkButton>
+          </div>
+          <div className="contact-page-copy">
+            <h2>
+              Facebook&nbsp;<i className="fa-brands fa-facebook"></i>
+            </h2>
+
+            <p>
+              Te dejo la página oficial de El Diablito Trece en Facebook. Visitanos y déjanos un
+              saludo en la bandeja de mensajes.
+            </p>
+            <LinkButton
+              to="https://www.facebook.com/eldiablitotrece"
+              target="_blank"
+              className="contact-page-CTA-button"
+            >
+              Ir a Facebook
+            </LinkButton>
+          </div>
         </div>
-        <LinkButton
-          to="https://discord.gg/pQAFQxaUWC"
-          target="_blank"
-          className="contact-page-CTA-button"
-        >
-          Ir a Discord
-        </LinkButton>
-        <div className="contact-page-copy">
+        <div className="contact-page-email-form">
           <h2>
-            Mánda un correo&nbsp;<i className="fa-solid fa-pencil"></i>
+            Mánda un correo&nbsp;<i className="fa-regular fa-envelope"></i>
           </h2>
           <EmailForm />
         </div>
